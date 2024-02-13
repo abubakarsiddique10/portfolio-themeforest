3480bdd045636801d648edf9d43e9e547ce113d7
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2024-02-13 09:42:35 +0600

    firefox issue solve

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,27 @@
 /* nav toggle button select*/
-const header = document.getElementById('header')
-/* const toggleBtn = document.querySelector('#nav__toggle'); */
-const toggleOpen = document.querySelector('#toggle__open');
-const toggleClose = document.querySelector('#toggle__close');
-const navRight = document.querySelector('.nav__menu')
-const navLogo = document.querySelector('.nav__logo')
-/* const navLink = document.querySelectorAll('.nav__link'); */
-const navMenu = document.querySelector('.nav__menu')
-const moon = document.querySelector('.moon');
-const sun = document.querySelector('.sun');
-const html = document.querySelector('html')
-const themeToggle = document.getElementById('theme__toggle');
-const headerTitle = document.querySelectorAll('.header__title')
-const isTheme = localStorage.getItem('theme');
-const hero = document.getElementById('hero');
-const servicesCard = document.querySelectorAll('.services__card');
-/* const testimonialCard = document.querySelectorAll('.testimonial__card'); */
-const priceCard = document.querySelectorAll('.price__card');
-const blogCard = document.querySelectorAll('.blog__card');
-const contactLeft = document.querySelector('.contact__left');
-const contactRight = document.querySelector('.contact__right');
-const messageInput = document.querySelectorAll('.contact__form input, textarea')
-const servicesCardSVG = document.querySelectorAll('.services__card svg');
+/* const header = document.getElementById('header') */
+/* const toggleOpen = document.querySelector('#toggle__open');
+const toggleClose = document.querySelector('#toggle__close'); */
+/* const navRight = document.querySelector('.nav__menu') */
+/* const navLogo = document.querySelector('.nav__logo') */
+/* const navMenu = document.querySelector('.nav__menu') */
+/* const moon = document.querySelector('.moon');
+const sun = document.querySelector('.sun'); */
+/* const html = document.querySelector('html') */
+/* const themeToggle = document.getElementById('theme__toggle');
+const headerTitle = document.querySelectorAll('.header__title') */
+/* const isTheme = localStorage.getItem('theme');
+const hero = document.getElementById('hero'); */
+/* const servicesCard = document.querySelectorAll('.services__card'); */
+/* const priceCard = document.querySelectorAll('.price__card');
+const blogCard = document.querySelectorAll('.blog__card'); */
+/* const contactLeft = document.querySelector('.contact__left');
+const contactRight = document.querySelector('.contact__right'); */
+/* const messageInput = document.querySelectorAll('.contact__form input, textarea') */
+/* const servicesCardSVG = document.querySelectorAll('.services__card svg'); */
 const portfolioWrappper = document.querySelector('.portfolio__wrappper')
-const testimonialCardSVG = document.querySelectorAll('.testimonial__card svg');
-const svg = document.querySelectorAll('svg:not(.card__rating svg)');
+/* const testimonialCardSVG = document.querySelectorAll('.testimonial__card svg'); */
+/* const svg = document.querySelectorAll('svg:not(.card__rating svg)'); */
 const preloader = document.getElementById('preloader');
 
 
@@ -67,119 +64,16 @@ window.addEventListener('scroll', () => {
 
 
 // Navbar active class
-const navLink = document.querySelectorAll('.nav-link');
+/* const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach((link) => {
     link.addEventListener('click', () => {
         document.querySelector('.nav-active').classList.remove('nav-active')
         link.classList?.add('nav-active')
     })
-})
+}) */
 /*=========================
     NAVBAR END
 ==========================*/
-
-/* 
-// nav background change function
-const maxWidth = window.matchMedia("(max-width: 992px)");
-const resizeScreen = () => {
-    const width = document.body.clientWidth;
-    const isTheme = localStorage.getItem('theme');
-
-    if (width <= 992 && isTheme == "dark") {
-        navMenu.style.background = "#242B33";
-    } else if (width > 992 && isTheme == "dark") {
-        navMenu.style.background = "transparent";
-    } else {
-        navMenu.style.background = "#FFFFFF";
-    }
-}
-window.addEventListener('resize', resizeScreen)
-
-// dark & light mood functionality
-if (isTheme == 'dark') {
-    html.classList.add('dark');
-    moon.style.display = "none";
-    sun.style.display = "block"
-    html.classList.remove('light');
-    hero.style.background = "#1D232A";
-
-    header.style.background = '#1D232A';
-    header.style.borderBottom = "1px solid rgba(166, 173, 186, 0.08)";
-    navLogo.style.color = "#A6ADBA";
-    navLink.forEach((item) => item.style.color = "#A6ADBA");
-    svg.forEach((item) => item.style.fill = " #A6ADBA");
-    servicesCardSVG.forEach((svg) => svg.style.fill = "#F0F0F6")
-
-    messageInput.forEach((input) => {
-        input.style.background = "#1D232A";
-        input.style.color = "#FFFFFF";
-    });
-
-    [...servicesCard, ...testimonialCard, ...priceCard, ...blogCard, contactLeft, contactRight, portfolioWrappper].forEach((card) => {
-        card.style.border = "1px solid rgba(166, 173, 186, 0.05)"
-        card.style.background = "#242B33";
-    });
-    resizeScreen()
-}
-console.log(testimonialCardSVG)
-themeToggle.addEventListener('click', () => {
-    const active = html.classList.toggle('dark');
-
-    if (active) {
-        localStorage.setItem('theme', 'dark');
-        moon.style.display = "none";
-        sun.style.display = "block";
-        html.classList.remove('light');
-        hero.style.background = "#1D232A";
-        header.style.background = '#1D232A';
-        header.style.borderBottom = "1px solid rgba(166, 173, 186, 0.08)";
-        navLogo.style.color = "#A6ADBA";
-        navLink.forEach((item) => item.style.color = "#A6ADBA");
-        svg.forEach((item) => item.style.fill = " #A6ADBA");
-        servicesCardSVG.forEach((svg) => svg.style.fill = "#F0F0F6")
-        messageInput.forEach((input) => {
-            input.style.background = "#1D232A";
-            input.style.color = "#FFFFFF";
-        });
-
-        [...servicesCard, ...testimonialCard, ...priceCard, ...blogCard, contactLeft, contactRight, portfolioWrappper].forEach((card) => {
-            card.style.border = "1px solid rgba(166, 173, 186, 0.05)"
-            card.style.background = "#242B33";
-        })
-        slideShow()
-        resizeScreen()
-
-    } else {
-        localStorage.setItem('theme', null);
-        moon.style.display = "block";
-        sun.style.display = "none";
-        html.classList.add('light');
-        hero.style.background = "linear-gradient(180deg,#f8fafc,rgba(242,202,252,.11) 34.69%,rgba(250,198,252,.11) 44.06%,rgba(175,183,255,.11) 54.48%,rgba(142,220,200,.07) 64.9%,#f8fafc 97.95%)";
-
-        header.style.background = '#FFFFFF';
-        header.style.borderBottom = "none";
-        navLogo.style.color = "#18191A";
-        navLink.forEach((item) => item.style.color = "#18191A");
-        svg.forEach((item) => item.style.fill = "#18191A");
-        servicesCardSVG.forEach((svg) => svg.style.fill = "#FFB400")
-        messageInput.forEach((input) => {
-            input.style.background = "#FFFFFF";
-            input.style.color = "black";
-        });
-
-        [...servicesCard, ...testimonialCard, ...priceCard, ...blogCard, contactLeft, contactRight, portfolioWrappper].forEach((card) => {
-            card.style.background = "rgb(248 250 252)";
-            card.style.border = "1px solid rgba(71, 85, 105, .1)";
-            card.style.borderRadius = "4px"
-        });
-        contactLeft.style.background = "#FFFFFF";
-        contactRight.style.background = "#FFFFFF";
-        slideShow()
-        resizeScreen()
-    }
-})
-*/
-
 
 /*=========================
     Portfolio Start
@@ -332,21 +226,6 @@ servicesModalBtn.addEventListener('click', () => {
 })
 /* Services End */
 
-
-
-const testbtn = document.querySelector('.test-btn');
-let des = document.getElementById('des');
-
-
-testbtn.addEventListener('click', () => {
-    fetchData()
-})
-
-async function fetchData() {
-    const response = await fetch('data.json');
-    const data = await response.json();
-    des.innerText = data[0].des
-}
 
 
 
